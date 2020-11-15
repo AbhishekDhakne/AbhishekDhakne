e84040b39883722efa858d31c2a139e9c876f189
AbhishekDhakne <abdhakne@mitaoe.ac.in>
2020-11-15 18:27:29 +0530

    master

---
+++ inter_web_s1/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
+
+$(".button-collapse").sidenav();
+
+$(document).ready(function(){
+  $('.scrollspy').scrollSpy({
+    scrollOfffset: 0
+  });
+});
